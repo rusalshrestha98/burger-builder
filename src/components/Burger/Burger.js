a9c9dca628dd43c071d0327b1fc231f5403a016f
Rusal Shrestha <rusalo101@gmail.com>
2021-02-19 20:22:59 -0500

    connecting state to build controls and updated CSS

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Burger.css'
-import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
     let arrayofIngredients = Object.keys(props.ingredients).map(igKey => {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,23 +3,63 @@ import Aux from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+// declaring object for the ingredient prices of each item
+// name global constants in all caps
+const INGREDIENT_PRICES = {
+    salad: 0.5, 
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     state = {
-        ingredients: {
+        ingredients: { // number of ingredients used
             salad: 0,
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4 // base price of the burger
     }
+
+    // when the user adds an ingredient
+    addIngredientHandler = (type) => {
+        // get the current ingredient count from the 'type' arg that is passed in
+        const oldCount = this.state.ingredients[type];
+        // update the count 
+        const updatedCount = oldCount + 1;
+        // create a new object with the spread operator with the updated count
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        // save the updated count of the ingredient in the new object
+        updatedIngredients[type] = updatedCount;
+        // update the total price with the ingredient prices added
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        // update the state
+        this.setState({
+            totalPrice: newPrice, 
+            ingredients: updatedIngredients
+        });
+    };
+
+    removeIngredientHandler = (type) => {
+
+    };
+
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler} 
+                />
             </Aux>
         )
-    }
-}
+    };
+};
 
-export default BurgerBuilder
+export default BurgerBuilder;
