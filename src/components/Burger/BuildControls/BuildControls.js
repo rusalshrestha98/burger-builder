cc1955db2980f60fa0c861e0ec9db8480d14062c
Rusal Shrestha <rusalo101@gmail.com>
2021-02-20 19:44:09 -0500

    displaying and updating the burger price + fixed bug in BuildControls.js array.map() function

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,15 +11,15 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(ctrl => {
+        <p> Current Price: <strong>${props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
                 removed={() => props.ingredientRemoved(ctrl.type)}
-                disabled={props.disabled[ctrl.type]}
-            />
-        })}
+                disabled={props.disabled[ctrl.type]}/>
+        ))}
     </div>
 )
 
