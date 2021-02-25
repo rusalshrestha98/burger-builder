8912fbc5af2a1c2c36a55f5b75598bc9a088aeca
Rusal Shrestha <rusalo101@gmail.com>
2021-02-24 22:07:55 -0500

    last commit was adding price to the order summary component; this commit is adding a toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
     <React.Fragment>
-        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <Toolbar></Toolbar>
         <main className={classes.Content}>
             {props.children}
         </main> 
